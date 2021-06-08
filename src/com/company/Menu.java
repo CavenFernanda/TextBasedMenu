3d249fa338dd43bea643d31c5c36d8824bef28e5
Caven Fernanda <fernanda17caven@gmail.com>
2021-06-08 15:30:40 +0200

    Removed menu 1

--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -3,44 +3,52 @@ import java.util.Scanner;
 
 public class Menu {
 
+    public static void displayMenu(int[] options, String[] menuFunction){
+        //Display Menu
+        System.out.println("*******\tM E N U\t*******");
+        System.out.println("---------------------------------------");
+        //Loop through the Menu Functions
+        for(int i = 0; i < options.length; i++){
+            System.out.printf("\n%d. %s",options[i],menuFunction[i]);
+        }
+        System.out.println("\n---------------------------------------");
+        System.out.print("Please enter choice: ");
+    }
+
     public static void main(String[] args) {
 
         //Declaring Scanner
         Scanner input = new Scanner(System.in);
 
-        //Display Menu
-        System.out.println("*******  M E N U  *******");
-        System.out.println("---------------------------------------");
-        System.out.println("1. List folders and files in a directory");
-        System.out.println("2. Display the contents of a file on screen");
-        System.out.println("3. Display HTML Response for a Web Address");
-        System.out.println("4. Exit Application\n");
-        System.out.println("Please enter choice: ");
+        // Data of the Menu
+        int[] options = {1,2,3,4};
+        String[] menuFunctions = {"List folders and files in a directory","Display the contents of a file on screen","Display HTML Response for a Web Address","Exit Application"};
+
+        //Exit Condition
+        int EXIT = options[options.length-1]; //Assume the last index will be how to exit programme
+
+
+        //display the menu
+        displayMenu(options,menuFunctions);
 
         //Getting input from the user
         int choice = input.nextInt();
 
-        while(choice != 4){
+        while(choice != EXIT){
 
             //User's choice
-            if (choice == 1){
-                System.out.println("List folders and files in a directory");
+            if (choice == options[0]){
+                System.out.println(menuFunctions[0]);
             }
-            else if (choice == 2){
-                System.out.println("Display the contents of a file on screen");
+            else if (choice == options[1]){
+                System.out.println(menuFunctions[1]);
             }
-            else if (choice == 3){
-                System.out.println("Display HTML Response for a Web Address");
+            else if (choice == options[2]){
+                System.out.println(menuFunctions[2]);
             }
 
-            //Display Menu
-            System.out.println("*******  M E N U  *******");
-            System.out.println("---------------------------------------");
-            System.out.println("1. List folders and files in a directory");
-            System.out.println("2. Display the contents of a file on screen");
-            System.out.println("3. Display HTML Response for a Web Address");
-            System.out.println("4. Exit Application\n");
-            System.out.println("Please enter choice: ");
+            //display the menu
+            displayMenu(options,menuFunctions);
 
             choice = input.nextInt();
         }
