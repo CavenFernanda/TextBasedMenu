aa786e6f5f3d59cd7520eb489a15803d04d172d3
Caven Fernanda <fernanda17caven@gmail.com>
2021-06-08 13:33:51 +0200

    Renamed Class to Menu. Got user input

---
+++ src/com/company/Menu.java
@@ -0,0 +1,35 @@
+package com.company;
+import java.util.Scanner;
+public class Menu {
+
+    public static void main(String[] args) {
+
+        //Declaring Scanner
+        Scanner input = new Scanner(System.in);
+
+        //Display Menu
+        System.out.println("*******  M E N U  *******");
+        System.out.println("---------------------------------------");
+        System.out.println("1. List folders and files in a directory");
+        System.out.println("2. Display the contents of a file on screen");
+        System.out.println("3. Display HTML Response for a Web Address");
+        System.out.println("4. Exit Application\n");
+        System.out.println("Please enter choice: ");
+
+        //Getting input from the user
+        int choice = input.nextInt();
+
+        if (choice == 1){
+            System.out.println("List folders and files in a directory");
+        }
+        else if (choice == 2){
+            System.out.println("Display the contents of a file on screen");
+        }
+        else if (choice == 3){
+            System.out.println("Display HTML Response for a Web Address");
+        }
+        else if (choice == 4){
+            System.out.println("You have Exited the program\nGoodbye!");
+        }
+    }
+}
