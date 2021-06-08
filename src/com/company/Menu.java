1d7edb08c4d103f21d2f6e8df0b25d2ca79916fa
Caven Fernanda <fernanda17caven@gmail.com>
2021-06-08 14:03:18 +0200

    Added Menu into loop

--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -1,5 +1,6 @@
 package com.company;
 import java.util.Scanner;
+
 public class Menu {
 
     public static void main(String[] args) {
@@ -19,17 +20,30 @@ public class Menu {
         //Getting input from the user
         int choice = input.nextInt();
 
-        if (choice == 1){
-            System.out.println("List folders and files in a directory");
+        while(choice != 4){
+
+            //User's choice
+            if (choice == 1){
+                System.out.println("List folders and files in a directory");
+            }
+            else if (choice == 2){
+                System.out.println("Display the contents of a file on screen");
+            }
+            else if (choice == 3){
+                System.out.println("Display HTML Response for a Web Address");
+            }
+
+            //Display Menu
+            System.out.println("*******  M E N U  *******");
+            System.out.println("---------------------------------------");
+            System.out.println("1. List folders and files in a directory");
+            System.out.println("2. Display the contents of a file on screen");
+            System.out.println("3. Display HTML Response for a Web Address");
+            System.out.println("4. Exit Application\n");
+            System.out.println("Please enter choice: ");
+
+            choice = input.nextInt();
         }
-        else if (choice == 2){
-            System.out.println("Display the contents of a file on screen");
-        }
-        else if (choice == 3){
-            System.out.println("Display HTML Response for a Web Address");
-        }
-        else if (choice == 4){
-            System.out.println("You have Exited the program\nGoodbye!");
-        }
+        System.out.println("You have ended the programme. Have a good day!");
     }
 }
