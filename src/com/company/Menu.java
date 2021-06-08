f3207ee6fff4963a6c4ce2b28c1244a78294bec8
Caven Fernanda <fernanda17caven@gmail.com>
2021-06-08 17:11:11 +0200

    User is prompted to enter a path on the local computer and press enter. The program would then print a list of files and folders contained in the specified directory to the screen

--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -1,57 +1,69 @@
 package com.company;
+
+import java.io.File;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Menu {
 
-    public static void displayMenu(int[] options, String[] menuFunction){
+    public static void displayMenu(int[] options, String[] menuFunction) {
         //Display Menu
         System.out.println("*******\tM E N U\t*******");
         System.out.println("---------------------------------------");
         //Loop through the Menu Functions
-        for(int i = 0; i < options.length; i++){
-            System.out.printf("\n%d. %s",options[i],menuFunction[i]);
+        for (int i = 0; i < options.length; i++) {
+            System.out.printf("\n%d. %s", options[i], menuFunction[i]);
         }
         System.out.println("\n---------------------------------------");
         System.out.print("Please enter choice: ");
     }
 
-    public static void main(String[] args) {
-
-        //Declaring Scanner
+    public static void directory() {
         Scanner input = new Scanner(System.in);
 
-        // Data of the Menu
-        int[] options = {1,2,3,4};
-        String[] menuFunctions = {"List folders and files in a directory","Display the contents of a file on screen","Display HTML Response for a Web Address","Exit Application"};
+        System.out.println("Directory: ");
+        String path = input.nextLine();
 
-        //Exit Condition
-        int EXIT = options[options.length-1]; //Assume the last index will be how to exit programme
+        File dir = new File(path);
+        for (File f : Objects.requireNonNull(dir.listFiles())) System.out.println(f.getName());
+    }
 
+        public static void main (String[]args){
 
-        //display the menu
-        displayMenu(options,menuFunctions);
+            //Declaring Scanner
+            Scanner input = new Scanner(System.in);
 
-        //Getting input from the user
-        int choice = input.nextInt();
+            // Data of the Menu
+            int[] options = {1, 2, 3, 4};
+            String[] menuFunctions = {"List folders and files in a directory", "Display the contents of a file on screen", "Display HTML Response for a Web Address", "Exit Application"};
 
-        while(choice != EXIT){
+            //Exit Condition
+            int EXIT = options[options.length - 1]; //Assume the last index will be how to exit programme
 
-            //User's choice
-            if (choice == options[0]){
-                System.out.println(menuFunctions[0]);
-            }
-            else if (choice == options[1]){
-                System.out.println(menuFunctions[1]);
-            }
-            else if (choice == options[2]){
-                System.out.println(menuFunctions[2]);
-            }
 
             //display the menu
-            displayMenu(options,menuFunctions);
+            displayMenu(options, menuFunctions);
 
-            choice = input.nextInt();
+            //Getting input from the user
+            int choice = input.nextInt();
+
+            while (choice != EXIT) {
+
+                //User's choice
+                if (choice == options[0]) {
+                    System.out.println(menuFunctions[0]);
+                    directory();
+                } else if (choice == options[1]) {
+                    System.out.println(menuFunctions[1]);
+                } else if (choice == options[2]) {
+                    System.out.println(menuFunctions[2]);
+                }
+
+                //display the menu
+                displayMenu(options, menuFunctions);
+
+                choice = input.nextInt();
+            }
+            System.out.println("You have ended the programme. Have a good day!");
         }
-        System.out.println("You have ended the programme. Have a good day!");
     }
-}
