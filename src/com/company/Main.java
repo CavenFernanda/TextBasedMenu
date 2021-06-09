f2d286c273fbffddc32a74e19bb3d119e112f94e
Caven Fernanda <fernanda17caven@gmail.com>
2021-06-09 03:04:18 +0200

    Prompt the user to enter a valid website address including protocol. then connect to the address and print the raw HTML to the screen.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,22 @@
 package com.company;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Objects;
 import java.util.Scanner;
 
-public class Menu {
+public class Main {
 
+    public static String getPath(){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter path: ");
+        String path = input.nextLine();
+        return path;
+    }
     public static void displayMenu(int[] options, String[] menuFunction) {
         //Display Menu
         System.out.println("*******\tM E N U\t*******");
@@ -20,34 +30,76 @@ public class Menu {
     }
 
     public static void displayDirectory() {
-        Scanner input = new Scanner(System.in);
-
-        System.out.print("Enter File directory: ");
-        String path = input.nextLine();
+        String path = getPath();
 
         File dir = new File(path);
         for (File f : Objects.requireNonNull(dir.listFiles())) {
             System.out.println(f.getName());
         }
+
     }
 
     public static void displayFile() {
 
-        Scanner input = new Scanner(System.in);
+        String fileName = getPath();
 
-        System.out.print("Enter your full file path: ");
-        String fileName = input.nextLine();
-
+        FileInputStream file;
         try {
-            Scanner inputFile = new Scanner(new File(fileName));
-
-            while(inputFile.hasNextLine()){
-                System.out.println(inputFile.nextLine());
-            }
+            file = new FileInputStream(fileName);
+            showFileContent(file);
         } catch (FileNotFoundException e) {
-            System.out.println("Could not find file path");
+            //TODO: handle this
         }
 
+    }
+
+    public static void showFileContent(InputStream file) {
+        BufferedReader fileOpen = new BufferedReader(
+                new InputStreamReader(file));
+
+        System.out.println("Here's your file:\r\n");
+
+        String urlString = "";
+        String current = "";
+
+        while(true) {
+            try {
+                if (!((current = fileOpen.readLine()) != null)) break;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            urlString += current;
+        }
+        System.out.println(urlString);
+    }
+
+    public static void displayWeb() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter URL: ");
+        String uri = input.nextLine();
+
+        try {
+            URL url = new URL(uri);
+            URLConnection urlConnection = url.openConnection();
+            HttpURLConnection connection = null;
+            if(urlConnection instanceof HttpURLConnection) {
+                connection = (HttpURLConnection) urlConnection;
+            }else {
+                System.out.println("Please enter an HTTP URL.");
+                return;
+            }
+
+            showFileContent(connection.getInputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String readInput(){
+
+        System.out.println("Enter a website address: ");
+        Scanner input = new Scanner(System.in);
+        return input.nextLine();
     }
 
     public static void main(String[] args) {
@@ -80,11 +132,13 @@ public class Menu {
                 displayFile();
             } else if (choice == options[2]) {
                 System.out.println(menuFunctions[2]);
+                displayWeb();
             }
 
             //display the menu
             displayMenu(options, menuFunctions);
 
+            //TODO: Add Exception handling for out of bounds option
             choice = input.nextInt();
         }
         System.out.println("You have ended the programme. Have a good day!");
